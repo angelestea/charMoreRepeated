be685540784c1679b9277f8b1596502efca274bc
angelestea <angelhidrobo2015@gmail.com>
2022-03-06 17:53:46 -0500

    updating

--- charMoreRepeated.js
+++ charMoreRepeated.js
@@ -1,10 +1,10 @@
 /*
-The objective is save the letter that exist more in whichever string, so the focus must be in the object json times, because you can save element with value in this ones.
+The objective is save the letter that exist more in whichever string, so the focus must be in the object json times, because you can save element with value in these ones.
 
 Enjoy!
 */
 function moreUsedChar(word){
-    
+    word=word.replace(/ /g,"").toLowerCase();
     let times={};
     let maximo_repeticiones =0;
     let letra_mas_repetida="";
@@ -33,7 +33,8 @@ function moreUsedChar(word){
             letra_mas_repetida=letra;
         }
     }
+
     return letra_mas_repetida; 
 }
 
-console.log("The letter than exist more in this string is: "+moreUsedChar("salomunsalomon"));
+console.log("The letter than exist more in this string is: '"+moreUsedChar("sal om  un sal om on ho la  si ud  bas ids  ba sdu o ad")+"'");
